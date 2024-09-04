78bb133806444ddfb8cd2f8c33866f65c56f9b55
codingfunz <showmanager.live@gmail.com>
2024-09-04 00:24:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,9 @@ include 'functions.php';
 
 <body <?php echo bodyClass(); ?>>
 
-<?php echo get(config()->config_page_head); ?>
-
 <?php 
+echo get(config()->config_page_head);
+
 // do forms
 if( isset($_GET['do_invoice']) || isset($_GET['do_configs']) ) { 
 	if( isset($_GET['do_invoice']) )
@@ -31,70 +31,11 @@ if( isset($_GET['do_invoice']) || isset($_GET['do_configs']) ) {
 	if( isset($_GET['do_configs']) )
 		include 'configs.php';
 }else{
-
-$paypal='';
-if( get(config()->config_paypal_key) ) {
-$paypal = smartCheckout([
-		'items_name' => (isset(invoice()->item) ? invoice()->item :''),
-		'items_description' => (isset(invoice()->item) ? invoice()->item :'')
-		]);
+	include 'frontpage.php';
 }
 
-$paid_class= $paid_html='';
-if( isset(invoice()->paid) && invoice()->paid ) {
-	$paid_class = ' is-paid';
-	$paid_html = '<div class="is-paid center pad5">Paid</div>';
-}
+echo get(config()->config_page_foot); 
 ?>
-
-
-<div class="pay-btn">
-	<form method="post" action="" id="regForm" autocomplete="off">
-		<?php if( payState() ) { ?>
-		<div id="user_fields">
-			<p class="alert alert-success">The payment was processed succesfully.</p>
-		</div>
-		<?php } ?>
-		
-		<?php if( !payState() ) { ?>
-		<div id="donation_group">
-			<div class="amount-field invoice-detail">
-			
-				<?php if( $invoice_exist ) { ?>
-				<input type="hidden" name="amount_field" value="<?php echo invoice()->amt; ?>" /> 
-				<h3 class="flex flex-between item-name<?php echo $paid_class; ?>">
-					<span><?php echo invoice()->item; ?></span>
-					<span>$<?php echo invoice()->dollar; ?></span>
-				</h3>
-				
-				<?php echo $paid_html; ?>
-				
-				<div class="item-detail">
-				<?php echo invoice()->client; ?>
-				<?php echo invoice()->number; ?>
-				<?php echo invoice()->note; ?>
-				<?php echo (!invoice()->paid ? $paypal : ''); ?>
-				<?php echo invoice()->detail; ?>
-				</div>
-				
-				<?php }else{ ?>
-				<div class="fund-block">
-					<div class="flex flexmid flexcenter mb40">
-						<span style="white-space: nowrap; margin-right: 10px; font-weight: 700;">Enter Amount</span> <span>$</span>
-						<input type="number" name="amount_field" id="other_amt" min="5" value="1.00" />
-					</div>
-					<?php echo $paypal; ?>
-				</div>
-				
-				<?php } ?>
-			</div>
-		</div>
-		
-		<?php } ?>
-	</form>
-</div>
-<?php } // end do form condition ?>
-<?php echo get(config()->config_page_foot); ?>
 
 </body>
 </html>
