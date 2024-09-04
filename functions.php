1db407572eb5344e7cd558b70c246df1f0d7aaf7
codingfunz <showmanager.live@gmail.com>
2024-09-04 00:23:29 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -464,3 +464,36 @@ function references() {
 	
 	return implode($list);
 }
+
+function sanitize($post)
+{
+	$keep = '<br><div><img><ul><ol><li><a><p><section><hr><h1><h2><h3><h4><h5><h6><span><strong><table><tr><td><tbody><tfoot><th>';
+	$data=[];
+	foreach($post as $key => $val) {
+		if( is_array($val) ) {
+			$data[$key] = $val;
+			continue;
+		}
+		$data[$key] = strip_tags($val,$keep);
+	}
+	
+	return $data;
+}
+
+function strip_tags_content($text, $tags = '', $invert = FALSE) 
+{
+	preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
+	$tags = array_unique($tags[1]);
+
+	if(is_array($tags) AND count($tags) > 0) {
+		if($invert == FALSE) {
+			return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
+		}else{
+			return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
+		}
+	}elseif($invert == FALSE) {
+		return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
+	}
+
+	return $text;
+}
